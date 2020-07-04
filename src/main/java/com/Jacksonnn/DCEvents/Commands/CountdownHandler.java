a8c7f4aaf232ec07dc4eef1b8a31b1fc7eac2caf
Jacksonnnnn <jackson.huse@stu.jefferson.kyschools.us>
2020-07-04 16:32:19 -0400

    Fixed logical errors in Countdown Command.

--- src/main/java/com/Jacksonnn/DCEvents/Commands/CountdownHandler.java
+++ src/main/java/com/Jacksonnn/DCEvents/Commands/CountdownHandler.java
@@ -23,17 +23,11 @@ public class CountdownHandler extends BukkitRunnable {
     public void run() {
         if (this.isRunning && this.ticksLeft-- % 20 == 0)
             if (this.ticksLeft <= 0) {
-                sendMessage(ChatColor.LIGHT_PURPLE.toString() + ChatColor.MAGIC + " ------ " + ChatColor.RESET + ChatColor.GREEN + ChatColor.BOLD + "GO!" + ChatColor.LIGHT_PURPLE + ChatColor.MAGIC + " ------ ");
+                Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE.toString() + ChatColor.MAGIC + " ------ " + ChatColor.RESET + ChatColor.GREEN + ChatColor.BOLD + "GO!" + ChatColor.LIGHT_PURPLE + ChatColor.MAGIC + " ------ ");
                 this.isRunning = false;
             } else {
                 String number = ((this.ticksLeft + 1) / 20) + "";
-                sendMessage(ChatColor.DARK_RED + " --" + ChatColor.RED + "--" + ChatColor.GOLD + "-- " + ChatColor.AQUA + ChatColor.ITALIC + number + ChatColor.GOLD + " --" + ChatColor.RED + "--" + ChatColor.DARK_RED + "-- ");
+                Bukkit.broadcastMessage(ChatColor.DARK_RED + " --" + ChatColor.RED + "--" + ChatColor.GOLD + "-- " + ChatColor.AQUA + ChatColor.ITALIC + number + ChatColor.GOLD + " --" + ChatColor.RED + "--" + ChatColor.DARK_RED + "-- ");
             }
-    }
-
-    private void sendMessage(String message) {
-        Bukkit.getServer().getConsoleSender().sendMessage(message);
-        for (Player player : Bukkit.getServer().getOnlinePlayers())
-            player.sendMessage(message);
     }
 }
